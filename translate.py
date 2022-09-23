58d7389404026706963b7afddef2e9058235d160
khm-wipfli <karl-henry.martinsson@wipfli.com>
2022-09-22 23:52:44 -0500

    Checking arguments, added unit testing

--- translate.py
+++ translate.py
@@ -9,15 +9,28 @@ language_translator = LanguageTranslatorV3(
 
 language_translator.set_service_url('https://api.us-east.language-translator.watson.cloud.ibm.com')
 
-
 def englishToFrench(englishText):
-    frenchText = language_translator.translate(
-    text=englishText,
-    model_id='en-fr').get_result()
+    # Check for null or blank string
+    if englishText == None:
+        return None
+    if englishText == "":
+        return ""
+    # Perform translation from English to French
+    translationJSON = language_translator.translate(
+        text=englishText,
+        model_id='en-fr').get_result()
+    frenchText = translationJSON[0].translations
     return frenchText
 
 def frenchToEnglish(frenchText):
-    englishText = language_translator.translate(
-    text=frenchText,
-    model_id='fr-en').get_result()
+    # Check for null or blank string
+    if frenchText == None:
+        return None
+    if frenchText == "":
+        return ""
+    # Perform translation from French to English
+    translationJSON = language_translator.translate(
+        text=frenchText,
+        model_id='fr-en').get_result()
+    englishText = translationJSON[0].translations
     return englishText
